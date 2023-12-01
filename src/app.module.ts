7325d42fbb1316ad2a4c12a1b008932b323a3897
keeepmin94 <wjdwkjh94@gmail.com>
2023-12-01 14:03:30 +0900

    feat: typeorm 세팅_mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import serverConfig from './config/server.config';
 import { validationSchema } from './config/validationSchema';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
@@ -11,6 +13,9 @@ import { validationSchema } from './config/validationSchema';
       envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
       validationSchema,
       cache: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfig,
     }),
   ],
   controllers: [],
--- src/config/validationSchema.ts
+++ src/config/validationSchema.ts
@@ -1,5 +1,10 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
-  SERVER_PORT: Joi.string().required(),
+  SERVER_PORT: Joi.number().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().required(),
+  DB_USER: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
 });
